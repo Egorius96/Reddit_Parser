0814a68d5645422f9e9e73c6a2ca75bd8c225487
Egor <Yegor.lob@yandex.ru>
2021-11-09 19:34:02 +0300

    add func get_html_code add func get_post_urls

--- Reddit_Parser.py
+++ Reddit_Parser.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 from selenium import webdriver
 
 
@@ -10,30 +9,43 @@ def spider():
     soup = BeautifulSoup(response.text, 'html.parser')
 
     with open('result.txt', 'w', encoding="utf-8") as result:
-        result.write(str(soup.encode("utf-8")))
-
-#spider()
+        result.write(str(soup.prettify())) #.encode("utf-8")))
 
 
 def get_html_code(url):
     driver = webdriver.Chrome(
-        executable_path='G:\YandexDisk\study\Python\PycharmProjects\Reddit_Parser\chromedriver.exe'
-    )
-
-    #driver.maximize_window()
-
+        executable_path='G:\YandexDisk\study\Python\PycharmProjects\Reddit_Parser\chromedriver.exe')
     try:
         driver.get(url=url)
-        time.sleep(3)
+        while True:
+            posts = 0
+            while posts <= 100:
+                posts = driver.page_source.count('_eYtD2XCVieq6emjKBH3m')
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            break
     except Exception as _ex:
         print(_ex)
     finally:
+        main_html_code = driver.page_source
         driver.close()
         driver.quit()
+        return main_html_code
+
+
+def get_post_urls(main_html_code):
+    post_urls = []
+    soup = BeautifulSoup(main_html_code, 'html.parser').find_all('a', attrs={'class' : '_3jOxDPIQ0KaOWpzvSQo-1s'})
+    for link in soup:
+        post_urls.append(link.get('href'))
+    return post_urls
 
 
 def main():
-    get_html_code(url='https://www.reddit.com/top/?t=month')
+    #spider()
+    main_html_code = get_html_code(url='https://www.reddit.com/top/?t=month')
+    post_urls = get_post_urls(main_html_code)
+    # with open('result.txt', 'w', encoding="utf-8") as result:
+    #     result.write(str(post_urls))
 
 
 if __name__ == "__main__":
