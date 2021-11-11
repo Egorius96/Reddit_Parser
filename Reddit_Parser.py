cc1f8bdfdb10aa647cf88dfdecc75427c111c198
Egor <Yegor.lob@yandex.ru>
2021-11-11 21:33:07 +0300

    Сollect the data, remove unnecessary functions
    using a Beautiful soup get the username, number of comments,
    number of votes, post category, post date

--- Reddit_Parser.py
+++ Reddit_Parser.py
@@ -1,15 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-
-
-def spider():
-    url = 'https://www.reddit.com/top/?t=month'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    with open('result.txt', 'w', encoding="utf-8") as result:
-        result.write(str(soup.prettify())) #.encode("utf-8")))
+from datetime import datetime, timedelta
 
 
 def get_html_code(url):
@@ -19,31 +11,62 @@ def get_html_code(url):
         driver.get(url=url)
         while True:
             posts = 0
-            while posts <= 100:
+            while posts <= 5:
                 posts = driver.page_source.count('_eYtD2XCVieq6emjKBH3m')
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            break
+            main_html_code = driver.page_source
+            return main_html_code
     except Exception as _ex:
         print(_ex)
     finally:
-        main_html_code = driver.page_source
         driver.close()
         driver.quit()
-        return main_html_code
 
 
-def get_post_urls(main_html_code):
-    post_urls = []
-    soup = BeautifulSoup(main_html_code, 'html.parser').find_all('a', attrs={'class' : '_3jOxDPIQ0KaOWpzvSQo-1s'})
+def get_user_urls(main_html_code):
+    user_url = []
+    soup = BeautifulSoup(main_html_code, 'html.parser').find_all('a', attrs={'class' : '_2tbHP6ZydRpjI44J3syuqC'})
     for link in soup:
-        post_urls.append(link.get('href'))
-    return post_urls
+        user_url.append('https://www.reddit.com/' + link.get('href'))
+    return user_url
+
+
+def get_data(main_html_code):
+    data = []
+    element = BeautifulSoup(main_html_code, 'html.parser').find_all('div', attrs={'class' : '_1oQyIsiPHYt6nx7VOmd1sz'})
+    for post in element:
+        try:
+            if len(data) <= 4:
+                data.append({
+                    'username' : post.find('a', attrs={'class' : '_2tbHP6ZydRpjI44J3syuqC'}).text[2:],
+                    'number of comments' : post.find('span', attrs={'class' : 'FHCV02u6Cp2zYL0fhQPsO'}).text.split(' ')[0],
+                    'number of votes': post.find('div', attrs={'class': '_1rZYMD_4xY3gRcSS3p8ODO'}).text,
+                    'post category': post.find('a', attrs={'class': '_3ryJoIoycVkA88fy40qNJc'}, text=True).text[2:],
+                })
+            else: pass
+        except Exception as _ex:
+            print(_ex)
+    return data
+
+def get_date(main_html_code):
+    element = BeautifulSoup(main_html_code, 'html.parser').find_all('div', attrs={'class': '_1oQyIsiPHYt6nx7VOmd1sz'})
+    for post in element:
+        try:
+            str = post.find('a', attrs={'class': '_3jOxDPIQ0KaOWpzvSQo-1s'}).text
+            if 'days' or 'day' in str:
+                date = (datetime.today() - timedelta(int(str.split(' ')[0]))).strftime('%Y %m %d')
+            else:
+                num = datetime.today().strftime('%Y %m %d')
+        except Exception as _ex:
+            print(_ex)
 
 
 def main():
-    #spider()
     main_html_code = get_html_code(url='https://www.reddit.com/top/?t=month')
-    post_urls = get_post_urls(main_html_code)
+    user_urls = get_user_urls(main_html_code)
+    # data = get_data(main_html_code)
+    print(user_urls)
+    # get_date(main_html_code)
     # with open('result.txt', 'w', encoding="utf-8") as result:
     #     result.write(str(post_urls))
 
